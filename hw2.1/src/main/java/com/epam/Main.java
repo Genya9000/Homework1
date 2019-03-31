1e30df0d4cd84d1bf70dab8c3ac208f7a535f24f
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-03-31 18:27:54 +0300

    task2

---
+++ hw2.1/src/main/java/com/epam/Main.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Eugene Khaliman");
+        Assert.fail("This is fail");
+    }
+}
+
